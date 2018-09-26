9336af0cbbfb25cdf40682ab8f0566a79e078684
justinpayan <jpayan@cs.umass.edu>
2018-09-26 12:48:40 -0400

    MAP scores

---
+++ expertise/evaluators/mean_avg_precision/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from .mean_avg_precision import Evaluator
---
+++ expertise/evaluators/mean_avg_precision/mean_avg_precision.py
@@ -0,0 +1,94 @@
+from __future__ import print_function, absolute_import
+
+import sys, os
+
+from operator import itemgetter
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use('ggplot')
+
+class Evaluator():
+    """
+    An Evaluator instance that evaluates
+    recall_at_m =
+        (number of papers reviewer bid positively on in top M) /
+        (total number of papers reviewer bid positively on)
+I think actually we want to evaluate against the list of reviewers suggested for a paper, rather than the list of papers suggested for a reviewer.
+
+    This evaluation method requires us to look at the bids, so we import them
+    from somewhere in the __init__() method.
+
+    """
+
+    def __init__(self, bids_by_forum, m=50):
+        # datapath = os.path.join(os.path.dirname(__file__), '../samples/uai_data')
+        # self.data = utils.load_obj(datapath)
+        # self.bids_by_forum = self.data['bids_by_forum']
+        self.bids_by_forum = bids_by_forum
+
+    def mean_avg_precision(self, ranked_signatures, forum):
+        '''
+        Among the top M users in the model's rank list,
+        compute the MAP.
+        Ranked signatures has [('paper_id', ['reviewer_name;affinity', ...]), (...), ...]
+
+        '''
+
+        positive_bids = self.get_pos_bids_for_forum(forum)
+        positive_signatures = [bid['signature'] for bid in positive_bids]
+
+        if not positive_signatures:
+            # what value should be returned if there are no positives?
+            return 1.0
+        else:
+            all_bids = self.get_all_bids_for_forum(forum)
+            bid_signatures = [bid['signature'] for bid in all_bids]
+
+            # only rank the reviewers who made a bid of some kind
+            # filtered_ranked_signatures = [r for r in ranked_signatures if r in bid_signatures]
+            # topM = filtered_ranked_signatures[:m]
+            precisions = []
+            correct = 0
+            total = 0
+            for r in ranked_signatures[:10]:
+                total += 1
+                if r in positive_signatures:
+                    correct += 1
+                precisions.append(correct/total)
+
+
+            return float(sum(precisions))/float(len(precisions))
+
+    def evaluate(self, ranklists):
+        """
+        Evaluate the model using a ranked list. Either you can evaluate using a single ranked list or
+        evaluate against each individual query and average their precision scores
+
+        Arguments
+            @ranklists: A list of tuples.
+            The 0th index of the tuple contains the forum ID of the rank list being evaluated.
+            The 1st index of the tuple contains a list of reviewer IDs, in order of expertise score.
+
+        Returns
+            a generator object that yields an array of scores for each ranked list. If only one
+            score is needed, return the score in an array by itself.
+        """
+
+        for forum, rank_list in ranklists:
+            ranked_signatures = [rank.split(";")[0] for rank in rank_list]
+            scores = []
+            scores.append(self.mean_avg_precision(ranked_signatures, forum))
+            yield forum, scores
+
+    def get_all_bids_for_forum(self, forum_id):
+        """ Returns all bids for the forum_id """
+        forum_bids = self.bids_by_forum[forum_id]
+        return [{"signature": bid.signatures[0], "bid": bid.tag} for bid in forum_bids]
+
+    def get_pos_bids_for_forum(self, forum_id):
+        """ Get all of the positive bids for a forum """
+        positive_labels = ["I want to review", "I can review"]
+        forum_bids = self.get_all_bids_for_forum(forum_id)
+        return [bid for bid in forum_bids if bid["bid"] in positive_labels]
