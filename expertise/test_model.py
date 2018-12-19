959569e915890b3d7e83f5c2f064c4c2a68eb556
Michael Spector <michaelspector@gmail.com>
2018-12-19 11:21:43 -0500

    add model test script

---
+++ expertise/test_model.py
@@ -0,0 +1,31 @@
+'''
+Model inference
+
+'''
+import argparse
+import importlib
+import os
+
+from expertise.utils.config import Config
+from expertise.utils.dataset import Dataset
+
+def test_model(config_path):
+    config_path = os.path.abspath(config_path)
+    experiment_path = os.path.dirname(config_path)
+
+    config = Config(config_path)
+
+    model = importlib.import_module(config.model)
+
+    test_path = os.path.join(experiment_path, 'test')
+    if not os.path.isdir(test_path):
+        os.mkdir(test_path)
+
+    model.test(config)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_path', help="a config file for a model")
+    args = parser.parse_args()
+
+    test_model(args.config_path)
