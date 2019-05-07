b6e7c7939324bd8d058245753e746609e4fa537e
Michael Spector <michaelspector@gmail.com>
2019-05-07 12:28:35 -0400

    add centroid_scibert model

---
+++ expertise/models/centroid_scibert/centroid_scibert.py
@@ -0,0 +1,227 @@
+import numpy as np
+
+# import fastText as ft
+
+import torch
+import torch.nn as nn
+from torch.nn import BCEWithLogitsLoss
+from expertise import utils
+
+from expertise.evaluators.mean_avg_precision import eval_map
+from expertise.evaluators.hits_at_k import eval_hits_at_k
+
+import ipdb
+
+class Model(torch.nn.Module):
+    def __init__(self, config, vocab):
+        super(Model, self).__init__()
+
+        self.config = config
+        self.vocab = vocab
+
+        # if self.config.fasttext:
+        #     self.cached_ft = ft.load_model(self.config.fasttext)
+        # else:
+        #     self.cached_ft = None
+
+        # Keyword embeddings
+        self.linear_layer = nn.Linear(config.bert_dim, config.embedding_dim)
+
+        # Vector of ones (used for loss)
+        if self.config.use_cuda:
+            self.ones = torch.ones(config.batch_size, 1).cuda()
+        else:
+            self.ones = torch.ones(config.batch_size, 1)
+
+        self._bce_loss = BCEWithLogitsLoss()
+
+    def compute_loss(self, batch_source, batch_pos, batch_neg):
+        """ Compute the loss (BPR) for a batch of examples
+        :param batch_source: a batch of source keyphrase indices (list of lists)
+        :param batch_pos: True aliases of the Mentions
+        :param batch_neg: False aliases of the Mentions
+        """
+
+        # B by dim
+        source_embed = self.embed(batch_source)
+        # B by dim
+        pos_embed = self.embed(batch_pos)
+        # B by dim
+        neg_embed = self.embed(batch_neg)
+
+        loss = self._bce_loss(
+            utils.row_wise_dot(source_embed, pos_embed )
+            - utils.row_wise_dot(source_embed, neg_embed ),
+            self.ones)
+        return loss
+
+    def score_pair(self, source, target, source_len, target_len):
+        """
+
+        :param source: Batchsize by Max_String_Length
+        :param target: Batchsize by Max_String_Length
+        :return: Batchsize by 1
+        """
+        source_embed = self.embed_dev(source, source_len)
+        target_embed = self.embed_dev(target, target_len)
+        scores = utils.row_wise_dot(source_embed, target_embed)
+        return scores
+
+    def embed(self, vector_batch):
+        """
+        :param vector_batch: torch.Tensor - Batch_size by bert_embedding_dim
+        """
+
+        if self.config.use_cuda:
+            vector_batch = vector_batch.cuda()
+
+        # do a linear transformation of the scibert vector into the centroid dimension
+        # B x bert_embedding_dim
+        try:
+            embeddings = self.linear_layer(vector_batch)
+        except AttributeError as e:
+            ipdb.set_trace()
+            raise e
+
+        return embeddings
+
+    def embed_dev(self, vector_batch, print_embed=False, batch_size=None):
+        """
+        :param keyword_lists: Batch_size by max_num_keywords
+        """
+        return self.embed(vector_batch)
+
+    def score_dev_test_batch(self,
+        batch_queries,
+        batch_targets,
+        batch_size
+        ):
+
+        if batch_size == self.config.dev_batch_size:
+            source_embed = self.embed_dev(batch_queries)
+            target_embed = self.embed_dev(batch_targets)
+        else:
+            source_embed = self.embed_dev(batch_queries, batch_size=batch_size)
+            target_embed = self.embed_dev(batch_targets, batch_size=batch_size)
+
+        scores = utils.row_wise_dot(source_embed, target_embed)
+
+        # what is this?
+        scores[scores != scores] = 0
+
+        return scores
+
+
+def generate_predictions(config, model, batcher, bert_lookup):
+    """
+    Use the model to make predictions on the data in the batcher
+
+    :param model: Model to use to score reviewer-paper pairs
+    :param batcher: Batcher containing data to evaluate (a DevTestBatcher)
+    :return:
+    """
+
+    for idx, batch in enumerate(batcher.batches(batch_size=config.dev_batch_size)):
+        if idx % 100 == 0:
+            print('Predicted {} batches'.format(idx))
+
+        batch_queries = []
+        batch_query_lengths = []
+        batch_query_ids = []
+        batch_targets = []
+        batch_target_lengths = []
+        batch_target_ids = []
+        batch_labels = []
+        batch_size = len(batch)
+
+        for data in batch:
+            # append a positive sample
+            batch_queries.append(bert_lookup[data['source_id']])
+            batch_query_lengths.append(data['source_length'])
+            batch_query_ids.append(data['source_id'])
+            batch_targets.append(bert_lookup[data['positive_id']])
+            batch_target_lengths.append(data['positive_length'])
+            batch_target_ids.append(data['positive_id'])
+            batch_labels.append(1)
+
+            # append a negative sample
+            batch_queries.append(bert_lookup[data['source_id']])
+            batch_query_lengths.append(data['source_length'])
+            batch_query_ids.append(data['source_id'])
+            batch_targets.append(bert_lookup[data['negative_id']])
+            batch_target_lengths.append(data['negative_length'])
+            batch_target_ids.append(data['negative_id'])
+            batch_labels.append(0)
+
+        scores = model.score_dev_test_batch(
+            torch.stack(batch_queries),
+            torch.stack(batch_targets),
+            np.asarray(batch_size)
+        )
+
+        if type(batch_labels) is not list:
+            batch_labels = batch_labels.tolist()
+
+        if type(scores) is not list:
+            scores = list(scores.cpu().data.numpy().squeeze())
+
+        for source, source_id, target, target_id, label, score in zip(
+            batch_queries,
+            batch_query_ids,
+            batch_targets,
+            batch_target_ids,
+            batch_labels,
+            scores
+            ):
+
+            # temporarily commenting out "source" and "target" because I think they are not needed.
+            prediction = {
+                # 'source': source,
+                'source_id': source_id,
+                # 'target': target,
+                'target_id': target_id,
+                'label': label,
+                'score': float(score)
+            }
+
+            yield prediction
+
+def load_jsonl(filename):
+
+    labels_by_forum = defaultdict(dict)
+    scores_by_forum = defaultdict(dict)
+
+    for data in utils.jsonl_reader(filename):
+        forum = data['source_id']
+        reviewer = data['target_id']
+        label = data['label']
+        score = data['score']
+        labels_by_forum[forum][reviewer] = label
+        scores_by_forum[forum][reviewer] = score
+
+
+    result_labels = []
+    result_scores = []
+
+    for forum, labels_by_reviewer in labels_by_forum.items():
+        scores_by_reviewer = scores_by_forum[forum]
+
+        reviewer_scores = list(scores_by_reviewer.items())
+        reviewer_labels = list(labels_by_reviewer.items())
+
+        sorted_labels = [label for _, label in sorted(reviewer_labels)]
+        sorted_scores = [score for _, score in sorted(reviewer_scores)]
+
+        result_labels.append(sorted_labels)
+        result_scores.append(sorted_scores)
+
+    return result_labels, result_scores
+
+def eval_map_file(filename):
+    list_of_list_of_labels, list_of_list_of_scores = utils.load_labels(filename)
+    return eval_map(list_of_list_of_labels, list_of_list_of_scores)
+
+def eval_hits_at_k_file(filename, k=2, oracle=False):
+    list_of_list_of_labels,list_of_list_of_scores = utils.load_labels(filename)
+    return eval_hits_at_k(list_of_list_of_labels, list_of_list_of_scores, k=k,oracle=oracle)
+
---
+++ expertise/models/centroid_scibert/train_centroid_scibert.py
@@ -0,0 +1,147 @@
+import datetime, os, sys, csv
+from shutil import copyfile, copytree
+import pickle
+
+import torch
+import torch.optim as optim
+import numpy as np
+
+from expertise.models import centroid_scibert
+
+from expertise import utils
+from expertise.utils.vocab import Vocab
+from expertise.utils.batcher import Batcher
+from expertise.utils.config import Config
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+#def train(setup_path, train_path, config, dataset):
+def train(config):
+
+    for train_subdir in ['dev_scores', 'dev_predictions']:
+        train_subdir_path = os.path.join(config.train_dir, train_subdir)
+        if not os.path.exists(train_subdir_path):
+            os.mkdir(train_subdir_path)
+
+    vocab_path = os.path.join(
+        config.centroid_samples_dir, 'vocab.pkl')
+    vocab = utils.load_pkl(vocab_path)
+
+    torch.manual_seed(config.random_seed)
+
+    train_samples_path = os.path.join(
+        config.centroid_samples_dir, 'train_samples_permuted.jsonl')
+
+    dev_samples_path = os.path.join(
+        config.centroid_samples_dir, 'dev_samples.jsonl')
+
+    print('reading train samples from ', train_samples_path)
+    batcher = Batcher(input_file=train_samples_path)
+    batcher_dev = Batcher(input_file=dev_samples_path)
+
+    model = centroid_scibert.Model(config, vocab)
+    if config.use_cuda:
+        model = model.cuda()
+
+    optimizer = optim.Adam(model.parameters(), lr=config.learning_rate, weight_decay=config.l2penalty)
+
+    # Stats
+    best_map = 0
+    sum_loss = 0.0
+
+    # a lookup table of torch.Tensor objects, keyed by user/paper ID.
+    bert_lookup = utils.load_pkl(os.path.join(config.setup_dir, 'bert_lookup.pkl'))
+
+    print('Begin Training')
+
+    # Training loop
+    for counter, batch in enumerate(batcher.batches(batch_size=config.batch_size)):
+
+        batch_source = []
+        batch_pos = []
+        batch_neg = []
+
+        for data in batch:
+            batch_source.append(bert_lookup[data['source_id']])
+            batch_pos.append(bert_lookup[data['positive_id']])
+            batch_neg.append(bert_lookup[data['negative_id']])
+
+        print('num_batches: {}'.format(counter))
+        optimizer.zero_grad()
+
+        loss_parameters = (
+            torch.stack(batch_source),
+            torch.stack(batch_pos),
+            torch.stack(batch_neg)
+        )
+
+        loss = model.compute_loss(*loss_parameters)
+        loss.backward()
+
+        # torch.nn.utils.clip_grad_norm(model.parameters(), config.clip)
+        optimizer.step()
+
+        # Question: is this if block just for monitoring?
+        if counter % 100 == 0:
+
+            this_loss = loss.cpu().data.numpy()
+            sum_loss += this_loss
+
+            print('Processed {} batches, Loss of batch {}: {}. Average loss: {}'.format(
+                counter, counter, this_loss, sum_loss / (counter / 100)))
+
+        if counter % config.eval_every == 0:
+
+            # is this reset needed?
+            batcher_dev.reset()
+
+            predictions = centroid_scibert.generate_predictions(config, model, batcher_dev, bert_lookup)
+
+            prediction_filename = config.train_save(predictions,
+                'dev_predictions/dev.predictions.{}.jsonl'.format(counter))
+
+            print('prediction filename', prediction_filename)
+            map_score = float(centroid_scibert.eval_map_file(prediction_filename))
+            hits_at_1 = float(centroid_scibert.eval_hits_at_k_file(prediction_filename, 1))
+            hits_at_3 = float(centroid_scibert.eval_hits_at_k_file(prediction_filename, 3))
+            hits_at_5 = float(centroid_scibert.eval_hits_at_k_file(prediction_filename, 5))
+            hits_at_10 = float(centroid_scibert.eval_hits_at_k_file(prediction_filename, 10))
+
+            score_lines = [
+                [config.name, counter, text, data] for text, data in [
+                    ('MAP', map_score),
+                    ('Hits@1', hits_at_1),
+                    ('Hits@3', hits_at_3),
+                    ('Hits@5', hits_at_5),
+                    ('Hits@10', hits_at_10)
+                ]
+            ]
+            config.train_save(score_lines, 'dev_scores/dev.scores.{}.tsv'.format(counter))
+
+            if map_score > best_map:
+                best_map = map_score
+
+                best_model_path = os.path.join(
+                    config.train_dir, 'model_{}_{}.torch'.format(config.name, 'best'))
+
+                torch.save(model, best_model_path)
+                config.best_model_path = best_model_path
+                config.best_map_score = best_map
+                config.hits_at_1 = hits_at_1
+                config.hits_at_3 = hits_at_3
+                config.hits_at_5 = hits_at_5
+                config.hits_at_10 = hits_at_10
+                config.save_config()
+
+                config.train_save(score_lines, 'dev.scores.best.tsv')
+
+        if counter == config.num_minibatches:
+            break
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_path', help='a config file for a model')
+    args = parser.parse_args()
+
+    train_model(args.config_path)
