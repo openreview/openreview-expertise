257269783d97d52d1d5cc84044ac1c7fdfcb188b
Carlos Mondragon <carlos@openreview.net>
2019-12-19 18:29:38 -0500

    Improve setup.py file to include all dependencies when installing with pip install

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        check_call('python -m spacy download en'.split())
+        check_call('python -m spacy download en_core_web_sm'.split())
+        develop.run(self)
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        check_call('python -m spacy download en'.split())
+        check_call('python -m spacy download en_core_web_sm'.split())
+        install.run(self)
 
 setup(
     name='openreview-expertise',
@@ -21,7 +38,13 @@ setup(
         'cloudpickle',
         'scikit-learn',
         'tqdm',
-        'pytorch_pretrained_bert'
+        'pytorch_pretrained_bert',
+        'ipdb',
+        'spacy'
     ],
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     zip_safe=False
 )
