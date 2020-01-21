fe947c07ed60839e6f551faed3c421a39fc3f1ae
Carlos Mondragon <carlos@openreview.net>
2020-01-21 09:32:11 -0500

    Remove unnecessary comments

--- expertise/config/core.py
+++ expertise/config/core.py
@@ -16,27 +16,6 @@ class ModelConfig(UserDict):
         elif kwargs.get('config_dict'):
             self.data = kwargs['config_dict']
 
-        # valid_model_names = expertise.available_models()
-        # if not 'model' in kwargs:
-        #     raise AttributeError(
-        #         f'ModelConfig requires a model. Select from {valid_model_names}')
-
-        # model = kwargs['model']
-
-        # if model not in valid_model_names:
-        #     raise ValueError(
-        #         f'"model" attribute must be one of {valid_model_names}')
-
-        # model_default_file = os.path.join(
-        #     expertise.model_importers()[model].path, model, f'{model}_default.json')
-
-        # with open(model_default_file) as f:
-        #     model_default_config = json.load(f, object_pairs_hook=OrderedDict)
-
-        # self._config = model_default_config
-
-        # self.update(**kwargs)
-
     def __repr__(self):
         return json.dumps(self.data, indent=4)
 
