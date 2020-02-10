7947e0412fa854cda509487393acbd9cc6cef50a
carlosmondra <carlos@openreview.net>
2020-02-10 14:52:46 -0500

    Feature/implement bm25 okapi (#32)
    * First working version of bm25
    
    * Implement bm25 and run file
    
    * Add multiprocessing

---
+++ expertise/run.py
@@ -0,0 +1,20 @@
+import argparse
+from pathlib import Path
+
+from .dataset import ArchivesDataset, SubmissionsDataset, BidsDataset
+from .config import ModelConfig
+from .models import bm25
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', help='a JSON file containing all other arguments')
+    args = parser.parse_args()
+
+    config = ModelConfig(config_file_path=args.config)
+    archives_dataset = ArchivesDataset(Path(config['dataset']['directory']).joinpath('archives'))
+    submissions_dataset = SubmissionsDataset(Path(config['dataset']['directory']).joinpath('submissions'))
+
+    if config['model'] == 'bm25':
+        bm25Model = bm25.Model(archives_dataset, submissions_dataset, use_title=config['model_params']['use_title'], use_abstract=config['model_params']['use_abstract'])
+        bm25Model.all_scores(Path(config['model_params']['scores_path']).joinpath(config['name'] + '.csv'))
+
