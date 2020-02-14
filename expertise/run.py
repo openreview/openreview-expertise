daed879638236d1615c2b2fcb31cc0f626c0eb3e
Carlos Mondragon <carlos@openreview.net>
2020-02-14 13:55:28 -0500

    Fix typos

--- expertise/run.py
+++ expertise/run.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     config = ModelConfig(config_file_path=args.config)
-    archives_dataset = ArchivesDataset(Path(config['dataset']['directory']).joinpath('archives'))
-    submissions_dataset = SubmissionsDataset(Path(config['dataset']['directory']).joinpath('submissions'))
+    archives_dataset = ArchivesDataset(archives_path=Path(config['dataset']['directory']).joinpath('archives'))
+    submissions_dataset = SubmissionsDataset(submissions_path=Path(config['dataset']['directory']).joinpath('submissions'))
 
     if config['model'] == 'bm25':
-        bm25Model = bm25.Model(archives_dataset, submissions_dataset, use_title=config['model_params'].get('use_title'), use_abstract=config['model_params'].get('use_abstract'), , workers=config['model_params'].get('workers'))
+        bm25Model = bm25.Model(archives_dataset, submissions_dataset, use_title=config['model_params'].get('use_title'), use_abstract=config['model_params'].get('use_abstract'), workers=config['model_params'].get('workers'))
         bm25Model.all_scores(Path(config['model_params']['scores_path']).joinpath(config['name'] + '.csv'))
 
     if config['model'] == 'elmo':
