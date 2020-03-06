56fe987f25fd96f2699a0d2bee244f85c31d7f64
Carlos Mondragon <carlos@openreview.net>
2020-03-06 15:09:16 -0500

    Add sparse_value to BM25

--- expertise/models/bm25/bm25.py
+++ expertise/models/bm25/bm25.py
@@ -2,9 +2,10 @@ import torch
 from tqdm import tqdm
 from rank_bm25 import BM25Okapi
 import multiprocessing
+import pickle
 
 class Model(object):
-    def __init__(self, archives_dataset, submissions_dataset, use_title=False, use_abstract=True, average_score=False, max_score=True, workers=1):
+    def __init__(self, archives_dataset, submissions_dataset, use_title=False, use_abstract=True, sparse_value=None, average_score=False, max_score=True, workers=1):
         if not average_score and not max_score:
             raise ValueError('average_score and max_score cannot both be False')
         if not use_title and not use_abstract:
@@ -18,6 +19,7 @@ class Model(object):
         self.use_abstract = use_abstract
         self.average_score = average_score
         self.max_score = max_score
+        self.sparse_value = sparse_value
         self.submissions_dataset = submissions_dataset
         self.title_corpus = []
         self.abstract_corpus = []
@@ -55,9 +57,13 @@ class Model(object):
         submission_scores = None
         reviewer_scores = {}
         if self.use_abstract:
+            if 'abstract' not in submission['content']:
+                return None
             tokenized_abstract = submission['content']['abstract'].lower().split(' ')
             submission_scores = torch.tensor(self.bm25_abstracts.get_scores(tokenized_abstract), dtype=torch.float32)
         elif self.use_title:
+            if 'title' not in submission['content']:
+                return None
             tokenized_title = submission['content']['title'].lower().split(' ')
             submission_scores = torch.tensor(self.bm25_titles.get_scores(tokenized_title), dtype=torch.float32)
         self.metadata['closest_match'][submission['id']] = (submission, self.raw_publications[submission_scores.max(dim=0)[1]])
@@ -82,12 +88,15 @@ class Model(object):
         csv_scores = []
         for note_id, submission in tqdm(submissions_dataset.items(), total=len(submissions_dataset)):
             reviewer_scores = self.score(submission)
+            if reviewer_scores is None:
+                continue
             for profile_id, score in reviewer_scores.items():
-                csv_line = '{note_id},{reviewer},{score}'.format(reviewer=profile_id, note_id=note_id, score=score)
+                csv_line = (note_id, profile_id, score)
+                # csv_line = '{note_id},{reviewer},{score}'.format(reviewer=profile_id, note_id=note_id, score=score)
                 csv_scores.append(csv_line)
         return csv_scores
 
-    def all_scores(self, scores_path=None):
+    def all_scores(self, preliminary_scores_path=None, scores_path=None):
         print('Computing all scores...')
         submissions_dicts = []
         submissions_dict = {}
@@ -98,13 +107,59 @@ class Model(object):
                 submissions_dict = {}
         submissions_dicts.append(submissions_dict)
         pool = multiprocessing.Pool(processes=self.workers)
-        csv_scores_list = pool.map(self.all_scores_helper, submissions_dicts)
+        scores_list = pool.map(self.all_scores_helper, submissions_dicts)
 
-        all_scores = []
+        self.preliminary_scores = []
+        for scores in scores_list:
+            self.preliminary_scores += scores
+
+        if preliminary_scores_path:
+            with open(preliminary_scores_path, 'wb') as f:
+                pickle.dump(self.preliminary_scores, f, pickle.HIGHEST_PROTOCOL)
+
         if scores_path:
             with open(scores_path, 'w') as f:
-                for csv_scores in csv_scores_list:
-                    all_scores += csv_scores
-                    for csv_line in csv_scores:
-                        f.write(csv_line + '\n')
+                for note_id, profile_id, score in self.preliminary_scores:
+                    f.write('{0},{1},{2}\n'.format(note_id, profile_id, score))
+        return self.preliminary_scores
+
+    def _sparse_scores_helper(self, all_scores, id_index):
+        counter = 0
+        # Get the first note_id or profile_id
+        current_id = self.preliminary_scores[0][id_index]
+        if id_index == 0:
+            desc = 'Note IDs'
+        else:
+            desc = 'Profiles IDs'
+        for note_id, profile_id, score in tqdm(self.preliminary_scores, total=len(self.preliminary_scores), desc=desc):
+            if counter < self.sparse_value:
+                all_scores.add((note_id, profile_id, score))
+            elif (note_id, profile_id)[id_index] != current_id:
+                counter = 0
+                all_scores.add((note_id, profile_id, score))
+                current_id = (note_id, profile_id)[id_index]
+            counter += 1
+        return all_scores
+
+    def sparse_scores(self, preliminary_scores_path=None, scores_path=None):
+        with open(preliminary_scores_path, 'rb') as f:
+            self.preliminary_scores = pickle.load(f)
+        print('Sorting...')
+        self.preliminary_scores.sort(key=lambda x: (x[0], x[2]), reverse=True)
+        all_scores = set()
+        # They are first sorted by note_id
+        all_scores = self._sparse_scores_helper(all_scores, 0)
+
+        # Sort by profile_id
+        print('Sorting...')
+        self.preliminary_scores.sort(key=lambda x: (x[1], x[2]), reverse=True)
+        all_scores = self._sparse_scores_helper(all_scores, 1)
+
+        print('Final Sort...')
+        all_scores = sorted(list(all_scores), key=lambda x: (x[0], x[2]), reverse=True)
+        if scores_path:
+            with open(scores_path, 'w') as f:
+                for note_id, profile_id, score in all_scores:
+                    f.write('{0},{1},{2}\n'.format(note_id, profile_id, score))
+
         return all_scores
