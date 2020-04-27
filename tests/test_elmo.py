0a3e2288dc4fb3bd1c0afd721401302b2e936bab
Carlos Mondragon <carlos@openreview.net>
2020-04-27 18:22:04 -0400

    Add tests for duplicate detection

--- tests/test_elmo.py
+++ tests/test_elmo.py
@@ -39,7 +39,7 @@ def test_elmo(tmp_path):
         submissions_path = tmp_path / 'submissions'
         submissions_path.mkdir()
         elmoModel.embed_publications(publications_path=publications_path.joinpath('pub2vec.pkl'))
-        elmoModel.embed_submssions(submissions_path=submissions_path.joinpath('sub2vec.pkl'))
+        elmoModel.embed_submissions(submissions_path=submissions_path.joinpath('sub2vec.pkl'))
 
     scores_path = tmp_path / 'scores'
     scores_path.mkdir()
@@ -48,3 +48,49 @@ def test_elmo(tmp_path):
         submissions_path=submissions_path.joinpath('sub2vec.pkl'),
         scores_path=scores_path.joinpath(config['name'] + '.csv')
     )
+
+def test_duplicate_detection(tmp_path):
+    submissions_dataset = SubmissionsDataset(submissions_path=Path('tests/data/submissions'))
+
+    config = {
+        'name': 'test_elmo_duplicates',
+        'model_params': {
+            'use_title': False,
+            'use_abstract': True,
+            'use_cuda': False,
+            'batch_size': 1,
+            'average_score': False,
+            'max_score': True,
+            'knn': 6,
+            'skip_elmo': False
+        }
+    }
+
+    other_submissions_dataset = False
+
+    elmoModel = elmo.Model(
+        use_title=config['model_params'].get('use_title', False),
+        use_abstract=config['model_params'].get('use_abstract', True),
+        use_cuda=config['model_params'].get('use_cuda', False),
+        batch_size=config['model_params'].get('batch_size', 4),
+        knn=config['model_params'].get('knn', 10),
+        skip_same_id=(not other_submissions_dataset)
+    )
+
+    if not config['model_params'].get('skip_elmo', False):
+        submissions_path = tmp_path / 'submissions'
+        submissions_path.mkdir()
+        elmoModel.set_submissions_dataset(submissions_dataset)
+        elmoModel.embed_submissions(submissions_path=submissions_path.joinpath('sub2vec.pkl'))
+
+    scores_path = tmp_path / 'scores'
+    scores_path.mkdir()
+    duplicates = elmoModel.find_duplicates(
+        submissions_path=submissions_path.joinpath('sub2vec.pkl'),
+        other_submissions_path=(Path(config['model_params']['other_submissions_path']).joinpath('osub2vec.pkl') if other_submissions_dataset else None),
+        scores_path=scores_path.joinpath(config['name'] + '.csv')
+    )
+    print(duplicates)
+    for sub_1, sub_2, score in duplicates:
+        if score > 0.99:
+            assert sub_1 == 'duplicate' or sub_2 == 'duplicate'
