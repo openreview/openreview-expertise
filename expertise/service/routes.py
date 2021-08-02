fcfd760fdbb0f888f5cb7d0947bf8f0ae956a657
haroldrubio <harrywill.rubio@gmail.com>
2021-08-02 17:58:57 -0400

    Update expertise endpoints

--- expertise/service/routes.py
+++ expertise/service/routes.py
@@ -5,19 +5,17 @@ import flask
 from flask_cors import CORS
 import threading
 import openreview
-from ..execute_expertise import *
+from .queue import ExpertiseQueue, UserPaperQueue, DatasetInfo
 
 
 BLUEPRINT = flask.Blueprint('expertise', __name__)
 CORS(BLUEPRINT, supports_credentials=True)
 
+task_queue = UserPaperQueue(max_jobs = 1, inner_queue=ExpertiseQueue)
+
 class ExpertiseStatusException(Exception):
     '''Exception wrapper class for errors related to the status of the Expertise model'''
     pass
-
-def expertise_pipeline(client, config):
-    execute_create_dataset(client, config_file=config)
-    execute_expertise(config_file=config)
 
 @BLUEPRINT.route('/expertise/test')
 def test():
@@ -27,7 +25,6 @@ def test():
 
 @BLUEPRINT.route('/expertise', methods=['POST'])
 def expertise():
-    flask.current_app.logger.debug('Create dataset request received')
     result = {}
 
     token = flask.request.headers.get('Authorization')
@@ -37,17 +34,110 @@ def expertise():
         result['error'] = 'No Authorization token in headers'
         return flask.jsonify(result), 400
     try:
-
+        config = flask.request.json['config']
         openreview_client = openreview.Client(
             token=token,
             baseurl=flask.current_app.config['OPENREVIEW_BASEURL']
         )
-        
-        thread = threading.Thread(
-            target = expertise_pipeline(openreview_client, flask.request.json['config'])
+        profile_id = openreview_client.get_profile().id
+        job_request = DatasetInfo(
+            id = profile_id,
+            job_name = config['name'],
+            config = config,
+            token = token,
+            baseurl = flask.current_app.config['OPENREVIEW_BASEURL']
         )
-        thread.start()
+        task_queue.put_job(job_request)
+        result = {
+            'user_id': profile_id,
+            'job_id': job_request.job_id
+        }
+    except openreview.OpenReviewException as error_handle:
+        flask.current_app.logger.error(str(error_handle))
 
+        error_type = str(error_handle)
+        status = 500
+
+        if 'not found' in error_type.lower():
+            status = 404
+        elif 'forbidden' in error_type.lower():
+            status = 403
+
+        result['error'] = error_type
+        return flask.jsonify(result), status
+
+    # pylint:disable=broad-except
+    except Exception as error_handle:
+        result['error'] = 'Internal server error: {}'.format(error_handle)
+        return flask.jsonify(result), 500
+        
+    else:
+        flask.current_app.logger.debug('POST returns ' + str(result))
+        return flask.jsonify(result), 200
+
+@BLUEPRINT.route('/jobs', methods=['GET'])
+def jobs():
+    result = {}
+
+    token = flask.request.headers.get('Authorization')
+    
+    if not token:
+        flask.current_app.logger.error('No Authorization token in headers')
+        result['error'] = 'No Authorization token in headers'
+        return flask.jsonify(result), 400
+    try:
+        openreview_client = openreview.Client(
+            token=token,
+            baseurl=flask.current_app.config['OPENREVIEW_BASEURL']
+        )
+        profile_id = openreview_client.get_profile().id
+        result = task_queue.get_jobs(profile_id)
+    except openreview.OpenReviewException as error_handle:
+        flask.current_app.logger.error(str(error_handle))
+
+        error_type = str(error_handle)
+        status = 500
+
+        if 'not found' in error_type.lower():
+            status = 404
+        elif 'forbidden' in error_type.lower():
+            status = 403
+
+        result['error'] = error_type
+        return flask.jsonify(result), status
+
+    # pylint:disable=broad-except
+    except Exception as error_handle:
+        result['error'] = 'Internal server error: {}'.format(error_handle)
+        return flask.jsonify(result), 500
+        
+    else:
+        flask.current_app.logger.debug('POST returns ' + str(result))
+        return flask.jsonify(result), 200
+
+@BLUEPRINT.route('/results', methods=['GET'])
+def results():
+    result = {}
+
+    token = flask.request.headers.get('Authorization')
+    
+    if not token:
+        flask.current_app.logger.error('No Authorization token in headers')
+        result['error'] = 'No Authorization token in headers'
+        return flask.jsonify(result), 400
+    try:
+        job_id = flask.request.json['job_id']
+        delete_on_get = flask.request.json['delete_on_get']
+        openreview_client = openreview.Client(
+            token=token,
+            baseurl=flask.current_app.config['OPENREVIEW_BASEURL']
+        )
+        profile_id = openreview_client.get_profile().id
+        result = task_queue.get_result(
+            profile_id,
+            delete_on_get = delete_on_get,
+            job_id = job_id
+        )
     except openreview.OpenReviewException as error_handle:
         flask.current_app.logger.error(str(error_handle))
 
