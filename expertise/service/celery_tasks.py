d9c19da6498609b4cfdda57d96f8901d86791158
haroldrubio <harrywill.rubio@gmail.com>
2021-08-25 10:13:53 -0400

    Move mock client function to utils.py

--- expertise/service/celery_tasks.py
+++ expertise/service/celery_tasks.py
@@ -1,44 +1,7 @@
 import logging, json, os, shutil
-from unittest.mock import MagicMock
+from .utils import mock_client
 from expertise.execute_expertise import *
 from expertise.service.server import celery_app as celery
-
-def mock_client():
-    client = MagicMock(openreview.Client)
-
-    def get_group(group_id):
-        with open('tests/data/fakeData.json') as json_file:
-            data = json.load(json_file)
-        group = openreview.Group.from_json(data['groups'][group_id])
-        return group
-
-    def search_profiles(confirmedEmails=None, ids=None, term=None):
-        with open('tests/data/fakeData.json') as json_file:
-            data = json.load(json_file)
-        profiles = data['profiles']
-        profiles_dict_emails = {}
-        profiles_dict_tilde = {}
-        for profile in profiles:
-            profile = openreview.Profile.from_json(profile)
-            if profile.content.get('emails') and len(profile.content.get('emails')):
-                profiles_dict_emails[profile.content['emails'][0]] = profile
-            profiles_dict_tilde[profile.id] = profile
-        if confirmedEmails:
-            return_value = {}
-            for email in confirmedEmails:
-                if profiles_dict_emails.get(email, False):
-                    return_value[email] = profiles_dict_emails[email]
-
-        if ids:
-            return_value = []
-            for tilde_id in ids:
-                return_value.append(profiles_dict_tilde[tilde_id])
-        return return_value
-
-    client.get_group = MagicMock(side_effect=get_group)
-    client.search_profiles = MagicMock(side_effect=search_profiles)
-
-    return client
 
 @celery.task(name='userpaper', track_started=True, bind=True, time_limit=3600 * 24)
 def run_userpaper(self, config: dict, logger: logging.Logger):
