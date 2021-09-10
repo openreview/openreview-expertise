12be6920121a21a735425defdabf2d1faa5bb391
haroldrubio <harrywill.rubio@gmail.com>
2021-09-10 11:47:48 -0400

    Remove unnecessary logging statements

--- expertise/service/celery_tasks.py
+++ expertise/service/celery_tasks.py
@@ -6,6 +6,7 @@ from expertise.service.server import celery_app as celery
 @celery.task(name='userpaper', track_started=True, bind=True, time_limit=3600 * 24)
 def run_userpaper(self, config: dict, logger: logging.Logger):
     try:
+        logger.info('Creating dataset')
         if 'token' in config.keys():
             openreview_client = openreview.Client(
                 token=config['token'],
@@ -13,33 +14,27 @@ def run_userpaper(self, config: dict, logger: logging.Logger):
             )
         else:
             openreview_client = mock_client()
-            logger.info('Creating dataset')
         execute_create_dataset(openreview_client, config=config)
         run_expertise.apply_async(
                 (config, logger),
                 queue='expertise',
         )
     except Exception as exc:
+        logger.error(f"Error in job: {config['job_id']}")
         working_dir = os.path.join(config['profile_dir'], str(config['job_id']))
-        logger.error(f'Removing dir: {working_dir}')
         shutil.rmtree(working_dir)
-        logger.error(f"Writing to: {os.path.join(config['profile_dir'], 'err.log')}")
         with open(os.path.join(config['profile_dir'], 'err.log'), 'a+') as f:
             f.write(f"{config['job_id']},{config['name']},{exc}\n")
-        logger.error('Error: {}'.format(exc))
 
 @celery.task(name='expertise', track_started=True, bind=True, time_limit=3600 * 24)
 def run_expertise(self, config: dict, logger: logging.Logger):
     try:
         logger.info('Executing expertise')
         execute_expertise(config=config)
-        logger.info('Returned from expertise')
     except Exception as exc:
+        logger.error(f"Error in job: {config['job_id']}")
         working_dir = os.path.join(config['profile_dir'], str(config['job_id']))
-        logger.error(f'Removing dir: {working_dir}')
         shutil.rmtree(working_dir)
-        logger.error(f"Writing to: {os.path.join(config['profile_dir'], 'err.log')}")
         with open(os.path.join(config['profile_dir'], 'err.log'), 'a+') as f:
             f.write(f"{config['job_id']},{config['name']},{exc}\n")
-        logger.error('Error: {}'.format(exc))
 
