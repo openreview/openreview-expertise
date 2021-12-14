3d47a1058f6ec34ddbebc351df86c7e69e3334a4
haroldrubio <harrywill.rubio@gmail.com>
2021-12-14 15:51:07 -0500

    Replace general test with journal format

--- tests/test_apiv2.py
+++ tests/test_apiv2.py
@@ -283,7 +283,7 @@ class TestExpertiseV2():
             }
         })
 
-    def test_request_expertise_with_valid_parameters(self, openreview_context, celery_session_app, celery_session_worker):
+    def test_journal_request_v2(self, openreview_context, celery_session_app, celery_session_worker):
         # Submit a working job and return the job ID
         MAX_TIMEOUT = 600 # Timeout after 10 minutes
         test_client = openreview_context['test_client']
@@ -293,8 +293,7 @@ class TestExpertiseV2():
             data = json.dumps({
                     'name': 'test_run',
                     'match_group': ["ABC.cc"],
-                    'paper_invitation': 'ABC.cc/-/Submission',
-                    'version': 2,
+                    'paper_id': 'KHnr1r7H',
                     "model": "specter+mfr",
                     "model_params": {
                         "use_title": False,
@@ -321,19 +320,18 @@ class TestExpertiseV2():
             time.sleep(5)
             response = test_client.get('/expertise/status', query_string={'id': f'{job_id}'}).json
             if response['status'] == 'Error':
-                assert False, response['description']
+                assert False, response[0]['description']
             try_time = time.time() - start_time
 
         assert try_time <= MAX_TIMEOUT, 'Job has not completed in time'
         assert response['status'] == 'Completed'
         assert response['name'] == 'test_run'
         assert response['description'] == 'Job is complete and the computed scores are ready'
-        assert response['cdate'] <= response['mdate']
         
         # Check config fields
         returned_config = response['config']
         assert returned_config['name'] == 'test_run'
-        assert returned_config['paper_invitation'] == 'ABC.cc/-/Submission'
+        assert returned_config['paper_id'] == 'KHnr1r7H'
         assert returned_config['model'] == 'specter+mfr'
         assert 'token' not in returned_config
         assert 'baseurl' not in returned_config
@@ -341,20 +339,23 @@ class TestExpertiseV2():
         assert job_id is not None
         openreview_context['job_id'] = job_id
 
-    def test_get_results_by_job_id(self, openreview_context, celery_session_app, celery_session_worker):
+    def test_get_journal_results(self, openreview_context, celery_session_app, celery_session_worker):
         test_client = openreview_context['test_client']
-        # Searches for results from the given job_id assuming the job has completed
-        response = test_client.get('/expertise/results', query_string={'id': f"{openreview_context['job_id']}", 'delete_on_get': True})
+        # Searches for journal results from the given job_id assuming the job has completed
+        response = test_client.get('/expertise/results', query_string={'id': f"{openreview_context['job_id']}"})
         metadata = response.json['metadata']
         assert metadata['submission_count'] == 2
         response = response.json['results']
         for item in response:
             submission_id, profile_id, score = item['submission'], item['user'], float(item['score'])
+            assert submission_id != 'dummy'
             assert len(submission_id) >= 1
             assert len(profile_id) >= 1
             assert profile_id.startswith('~')
             assert score >= 0 and score <= 1
-
+        
+        # Clean up journal request
+        response = test_client.get('/expertise/results', query_string={'id': f"{openreview_context['job_id']}", 'delete_on_get': True}).json['results']
         assert not os.path.isdir(f"./tests/jobs/{openreview_context['job_id']}")
         # Clean up directory
         shutil.rmtree(f"./tests/jobs/")
