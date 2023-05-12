d025d283035e27ed995c856953925cc244a2498a
Harold Rubio <43583679+haroldrubio@users.noreply.github.com>
2023-05-12 09:56:49 -0400

    Query by Paper ID and Match Group (#153)
    * Add paper ID test
    
    * Move and fix test
    
    * Add tests for joint query
    
    * Fix permissions bug
    
    * Add status all logic to status endpoint
    
    * Add search by entity prefix

--- tests/test_expertise_apiv2.py
+++ tests/test_expertise_apiv2.py
@@ -211,7 +211,79 @@ class TestExpertiseV2():
         assert req['entityA']['memberOf'] == 'TMLR/Action_Editors'
         assert req['entityB']['type'] == 'Note'
         assert req['entityB']['id'] == target_id
+
         openreview_context['job_id'] = job_id
+
+        # Make a request for TMLR/Reviewers
+        response = test_client.post(
+            '/expertise',
+            data = json.dumps({
+                    "name": "test_run",
+                    "entityA": {
+                        'type': "Group",
+                        'memberOf': "TMLR/Reviewers",
+                    },
+                    "entityB": { 
+                        'type': "Note",
+                        'id': target_id
+                    },
+                    "model": {
+                            "name": "specter+mfr",
+                            'useTitle': False, 
+                            'useAbstract': True, 
+                            'skipSpecter': False,
+                            'scoreComputation': 'avg'
+                    }
+                }
+            ),
+            content_type='application/json',
+            headers=openreview_client.headers
+        )
+        assert response.status_code == 200, f'{response.json}'
+        job_id = response.json['jobId']
+        time.sleep(2)
+        response = test_client.get('/expertise/status', query_string={'jobId': f'{job_id}'}).json
+        assert response['name'] == 'test_run'
+        assert response['status'] != 'Error'
+
+        # Query until job is complete
+        response = test_client.get('/expertise/status', query_string={'jobId': f'{job_id}'}).json
+        start_time = time.time()
+        try_time = time.time() - start_time
+        while response['status'] != 'Completed' and try_time <= MAX_TIMEOUT:
+            time.sleep(5)
+            response = test_client.get('/expertise/status', query_string={'jobId': f'{job_id}'}).json
+            if response['status'] == 'Error':
+                assert False, response[0]['description']
+            try_time = time.time() - start_time
+
+        assert try_time <= MAX_TIMEOUT, 'Job has not completed in time'
+        assert response['status'] == 'Completed'
+        assert response['name'] == 'test_run'
+        assert response['description'] == 'Job is complete and the computed scores are ready'
+
+        # Test for paper id query
+        response = test_client.get('/expertise/status', query_string={'paperId': target_id}).json['results']
+        assert len(response) == 2
+
+        # Test for paper id and member of query
+        response = test_client.get('/expertise/status', query_string={'paperId': target_id, 'memberOf': 'TMLR/Reviewers'}).json['results']
+        assert len(response) == 1
+
+        response = test_client.get('/expertise/status', query_string={'entityB.paperId': target_id, 'memberOf': 'TMLR/Reviewers'}).json['results']
+        assert len(response) == 1
+
+        response = test_client.get('/expertise/status', query_string={'entityB.paperId': target_id, 'entityB.memberOf': 'TMLR/Reviewers'}).json['results']
+        assert len(response) == 0
+
+        response = test_client.get('/expertise/status', query_string={'entityA.paperId': target_id}).json['results']
+        assert len(response) == 0
+
+        response = test_client.get('/expertise/status', query_string={'paperId': 'DoesNotExist'}).json['results']
+        assert len(response) == 0
+
+        response = test_client.get('/expertise/results', query_string={'jobId': f'{job_id}', 'deleteOnGet': True})
+        assert not os.path.isdir(f"./tests/jobs/{job_id}")
 
     def test_get_journal_results(self, openreview_context, celery_session_app, celery_session_worker):
         test_client = openreview_context['test_client']
