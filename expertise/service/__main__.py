58d8a69fe2d5fdd206e0dea3858e7c5bae9ecef2
haroldrubio <harrywill.rubio@gmail.com>
2024-02-20 03:44:53 -0500

    Add flag to block on artifacts

--- expertise/service/__main__.py
+++ expertise/service/__main__.py
@@ -9,9 +9,14 @@ if __name__ == "__main__":
     parser.add_argument('--host', default='localhost')
     parser.add_argument('--port', default=5000, type=int)
     parser.add_argument('--container', action='store_true')
+    parser.add_argument('--block_on_artifacts', action='store_true')
     args = parser.parse_args()
 
     if args.container:
-        threading.Thread(target=load_model_artifacts).start()
+        if args.block_on_artifacts: ## Batch prediction does not call /startup
+            load_model_artifacts()
+        else:
+            threading.Thread(target=load_model_artifacts).start()
+
 
     app.run(host=args.host, port=args.port)
