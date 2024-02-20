e6ad0bf9d4ce447674032f1cdfc7d24ec210c8d2
haroldrubio <harrywill.rubio@gmail.com>
2024-02-20 03:54:14 -0500

    Rollback blocking

--- expertise/service/__main__.py
+++ expertise/service/__main__.py
@@ -9,14 +9,10 @@ if __name__ == "__main__":
     parser.add_argument('--host', default='localhost')
     parser.add_argument('--port', default=5000, type=int)
     parser.add_argument('--container', action='store_true')
-    parser.add_argument('--block_on_artifacts', action='store_true')
     args = parser.parse_args()
 
     if args.container:
-        if args.block_on_artifacts: ## Batch prediction does not call /startup
-            load_model_artifacts()
-        else:
-            threading.Thread(target=load_model_artifacts).start()
+        threading.Thread(target=load_model_artifacts).start()
 
 
     app.run(host=args.host, port=args.port)
