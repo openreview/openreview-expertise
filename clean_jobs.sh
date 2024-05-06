d426748f5aab4bf7d39455fdce650ab7a521d6ff
haroldrubio <harrywill.rubio@gmail.com>
2024-05-06 08:09:44 -0400

    Delete all jobs older than 1 day

--- clean_jobs.sh
+++ clean_jobs.sh
@@ -1,7 +1,1 @@
-find /home/openreview/openreview-expertise/jobs/*/archives -prune -type d -mtime +2 -exec rm -r {} \;
-find /home/openreview/openreview-expertise/jobs/*/mfr -prune -type d -mtime +2 -exec rm -r {} \;
-find /home/openreview/openreview-expertise/jobs/*/specter -prune -type d -mtime +2 -exec rm -r {} \;
-find /home/openreview/openreview-expertise/jobs/*/sub2vec.jsonl -prune -mtime +2 -exec rm -r {} \;
-find /home/openreview/openreview-expertise/jobs/*/metadata.json -prune -mtime +2 -exec rm -r {} \;
-find /home/openreview/openreview-expertise/jobs/*/submissions.json -prune -mtime +2 -exec rm -r {} \;
-find /home/openreview/openreview-expertise/jobs/*/pub2vec.jsonl -prune -mtime +2 -exec rm -r {} \;
+find /home/openreview/openreview-expertise/jobs/* -prune -type d -mmin +1440 -exec rm -r {} \;
