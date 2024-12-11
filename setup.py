7ef18cecf233f1a2b428fb47614d73a6b13b8abf
Harold Rubio <harrywill.rubio@gmail.com>
2024-12-11 12:41:18 -0500

    Try fix gensim tokenizers

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'adapter-transformers==3.2.1post0',
         'pandas',
         'nltk',
-        'gensim==4.1.2',
+        'gensim>=4.2.0',
         'torch',
         'cloudpickle',
         'scikit-learn',
