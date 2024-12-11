2fcd59c6bce1e7ff1854c2540a78581745b71176
Harold Rubio <harrywill.rubio@gmail.com>
2024-12-11 10:41:25 -0500

    Back to 3.12

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     install_requires=[
         'openreview-py>=1.0.1',
-        'numpy==1.24.4',
+        'numpy==1.26.4',
         'scipy==1.10.1',
         'adapter-transformers==3.2.1post0',
         'pandas',
