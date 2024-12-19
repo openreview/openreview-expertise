57d13a5239f12f6844b3ec9a5e39448ead4e401d
Harold Rubio <harrywill.rubio@gmail.com>
2024-12-19 10:55:40 -0500

    Remove sleeps

--- tests/test_expertise_apiv2.py
+++ tests/test_expertise_apiv2.py
@@ -352,7 +352,6 @@ class TestExpertiseV2():
         )
         assert response.status_code == 200, f'{response.json}'
         job_id = response.json['jobId']
-        time.sleep(2)
         response = test_client.get('/expertise/status', headers=openreview_client.headers, query_string={'jobId': f'{job_id}'}).json
         assert response['name'] == 'test_run'
         assert response['status'] != 'Error'
@@ -406,7 +405,6 @@ class TestExpertiseV2():
         )
         assert response.status_code == 200, f'{response.json}'
         job_id = response.json['jobId']
-        time.sleep(2)
         response = test_client.get('/expertise/status', headers=openreview_client.headers, query_string={'jobId': f'{job_id}'}).json
         assert response['name'] == 'test_run'
         assert response['status'] != 'Error'
@@ -508,7 +506,6 @@ class TestExpertiseV2():
         )
         assert response.status_code == 200, f'{response.json}'
         job_id = response.json['jobId']
-        time.sleep(2)
         response = test_client.get('/expertise/status', headers=openreview_client.headers, query_string={'jobId': f'{job_id}'}).json
         assert response['name'] == 'test_run'
         assert response['status'] != 'Error'
