c2c36973b5d4bbcb09a01e16397a09fd1fcb848c
Harold Rubio <harrywill.rubio@gmail.com>
2025-03-17 18:27:00 -0400

    Move JSON setting

--- tests/generate_token.py
+++ tests/generate_token.py
@@ -1,16 +1,28 @@
 import openreview
 import os
 import sys
+import json
 
-def generate_token():
+# Helper script to generate a token for the container tests
+# and populate an example JSON file with the token
+
+# Writes out the test JSON to test_input.json
+
+def generate_token(path_to_json):
     try:
         client = openreview.api.OpenReviewClient(
             "http://localhost:3001", 
             username="openreview.net", 
             password="Or$3cur3P@ssw0rd"
         )
-        with open("api_token.txt", "w") as f:
-            f.write(client.token)
+
+        # Load JSON, write token to dict and write JSON to disk
+        with open(path_to_json, "r") as f:
+            data = json.load(f)
+        data["token"] = client.token
+        with open("test_input.json", "w") as f:
+            json.dump(data, f)
+
         print("Token generated successfully")
         return 0
     except Exception as e:
@@ -18,4 +30,4 @@ def generate_token():
         return 1
 
 if __name__ == "__main__":
-    sys.exit(generate_token())
+    sys.exit(generate_token(sys.argv[1]))
