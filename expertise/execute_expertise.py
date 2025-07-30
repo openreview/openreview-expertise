744527bb6fbeeea9b48ffc91dce8270a88da643e
Harold Rubio <43583679+haroldrubio@users.noreply.github.com>
2025-07-30 12:22:08 -0400

    Feature: Allow disable normalization (#259)
    * Pass keyword to models
    
    * Implement model-level logic
    
    * Add tests

--- expertise/execute_expertise.py
+++ expertise/execute_expertise.py
@@ -85,7 +85,8 @@ def execute_expertise(config):
             sparse_value=config['model_params'].get('sparse_value'),
             compute_paper_paper=config['model_params'].get('compute_paper_paper', False),
             venue_specific_weights=venue_specific_weights,
-            percentile_select=config['model_params'].get('percentile_select', None)
+            percentile_select=config['model_params'].get('percentile_select', None),
+            normalize_scores=config['model_params'].get('normalize_scores', True)
         )
         ens_predictor.set_archives_dataset(archives_dataset)
         ens_predictor.set_submissions_dataset(submissions_dataset)
@@ -124,7 +125,8 @@ def execute_expertise(config):
             sparse_value=config['model_params'].get('sparse_value'),
             dump_p2p=config['model_params'].get('dump_p2p', False),
             compute_paper_paper=config['model_params'].get('compute_paper_paper', False),
-            percentile_select=config['model_params'].get('percentile_select', None)
+            percentile_select=config['model_params'].get('percentile_select', None),
+            normalize_scores=config['model_params'].get('normalize_scores', True)
         )
         scincl_predictor.set_archives_dataset(archives_dataset)
         scincl_predictor.set_submissions_dataset(submissions_dataset)
@@ -159,7 +161,8 @@ def execute_expertise(config):
             sparse_value=config['model_params'].get('sparse_value'),
             dump_p2p=config['model_params'].get('dump_p2p', False),
             compute_paper_paper=config['model_params'].get('compute_paper_paper', False),
-            percentile_select=config['model_params'].get('percentile_select', None)
+            percentile_select=config['model_params'].get('percentile_select', None),
+            normalize_scores=config['model_params'].get('normalize_scores', True)
         )
         spec2_predictor.set_archives_dataset(archives_dataset)
         spec2_predictor.set_submissions_dataset(submissions_dataset)
