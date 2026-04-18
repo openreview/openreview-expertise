102b84bdda8cabf02f3e4c47dec6a5e344ebdd82
Melisa Bok <545506+melisabok@users.noreply.github.com>
2026-04-17 20:23:25 -0400

    remove dead code

--- expertise/models/centroid_scibert/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-from .centroid_scibert import *
-from .setup_centroid_scibert import setup
-from .train_centroid_scibert import train
-from .test_centroid_scibert import test
