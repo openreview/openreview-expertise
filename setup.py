a0610956901ec60e40025864ae23460bef1768de
Christopher <cjp.agostino@gmail.com>
2026-06-26 11:09:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='openreview-expertise',
-    version='2.2.4',
+    version='2.2.5',
     description='OpenReview paper-reviewer affinity modeling',
     url='https://github.com/openreview/openreview-expertise',
     author='OpenReview',
