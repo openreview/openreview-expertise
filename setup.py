368f47a9fd80436d415f21f8d905ba0832d57fb3
Christopher Agostino <cjp.agostino@gmail.com>
2026-08-18 15:41:14 -0400

    openrev py version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     install_requires=[
         'setuptools>=75.6.0',
-        'openreview-py>=2.5.0',
+        'openreview-py>=2.4.2',
         'numpy>=2.0,<2.3',
         'scipy',
         'adapter-transformers',
